901dd70271353885ee4a4610556750710c0eaffc
Chunmeng.Hua <chunmeng.hua@faureciacoagent.com>
2018-09-01 13:32:03 +0800

    Add VideoEncodeActivity for later use

---
+++ app/src/main/java/com/hua/testlibs/VideoEncodeActivity.java
@@ -0,0 +1,13 @@
+package com.hua.testlibs;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class VideoEncodeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_encode);
+    }
+}
